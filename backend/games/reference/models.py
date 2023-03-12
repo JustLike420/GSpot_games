dbb9a9833d39dd874ef87b73d855aefa3d1b94ab
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-03-12 13:20:57 +0700

    create model GroupProduct

--- backend/games/reference/models.py
+++ backend/games/reference/models.py
@@ -1,28 +1,29 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from core.models import Product
 
 
 class Group(models.Model):
 
     class TypeGroup(models.TextChoices):
         GENRE = 'G', _('Genre')
-        CATEGORY = 'C', _('Categoty')
+        CATEGORY = 'C', _('Category')
         SPECIAL_SECTION = 'SS', _('Special_section')
         PLAYER_NUMBER = 'PN', _('Player_numbers')
         THEME = 'T', _('Theme')
 
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField('Группа', max_length=256)
     type = models.CharField(max_length=1, choices=TypeGroup.choices)
 
     class Meta:
-        verbose_name = 'группа'
-        verbose_name_plural = 'группы'
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
 class GroupElement(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=256)
+    name = models.CharField('Подгруппа', max_length=256)
     group_id = models.ForeignKey(
         Group,
         related_name='group_element',
@@ -31,8 +32,17 @@ class GroupElement(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Элемент группы'
+        verbose_name = 'Подгруппы'
 
 
 class GroupProduct(models.Model):
-    ...
+    id = models.BigAutoField(primary_key=True)
+    game_id = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, verbose_name='Игра'
+    )
+    group_element_id = models.ForeignKey(
+        GroupElement, on_delete=models.SET_NULL, verbose_name='Подгруппа'
+    )
+
+    class Meta:
+        default_related_name = 'group_product'
