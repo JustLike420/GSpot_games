db016d0c5802bca230e1dff7ff5426d186914b82
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-03-12 12:59:05 +0700

    create model GroupElement

--- backend/games/reference/models.py
+++ backend/games/reference/models.py
@@ -6,12 +6,12 @@ class Group(models.Model):
 
     class TypeGroup(models.TextChoices):
         GENRE = 'G', _('Genre')
-        CATEGORY = 'C', _('categoty')
+        CATEGORY = 'C', _('Categoty')
         SPECIAL_SECTION = 'SS', _('Special_section')
         PLAYER_NUMBER = 'PN', _('Player_numbers')
-        THEME = 'GR', _('Graduate')
+        THEME = 'T', _('Theme')
 
-    id = models.IntegerField('Первичный ключ группы', primary_key=True)
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=256)
     type = models.CharField(max_length=1, choices=TypeGroup.choices)
 
@@ -21,7 +21,17 @@ class Group(models.Model):
 
 
 class GroupElement(models.Model):
-    ...
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=256)
+    group_id = models.ForeignKey(
+        Group,
+        related_name='group_element',
+        on_delete=models.SET_DEFAULT,
+        default='Unknown',
+    )
+
+    class Meta:
+        verbose_name = 'Элемент группы'
 
 
 class GroupProduct(models.Model):
