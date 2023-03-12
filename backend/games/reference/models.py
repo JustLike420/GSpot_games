117ab8d786cec6dcf07ce1980c95ac9ae817939b
Никита Ефремов <nikita_efremov_1995.09.14@mail.ru>
2023-03-12 12:32:21 +0700

    create model group

--- backend/games/reference/models.py
+++ backend/games/reference/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Group(models.Model):
+
+    class TypeGroup(models.TextChoices):
+        GENRE = 'G', _('Genre')
+        CATEGORY = 'C', _('categoty')
+        SPECIAL_SECTION = 'SS', _('Special_section')
+        PLAYER_NUMBER = 'PN', _('Player_numbers')
+        THEME = 'GR', _('Graduate')
+
+    id = models.IntegerField('Первичный ключ группы', primary_key=True)
+    name = models.CharField(max_length=256)
+    type = models.CharField(max_length=1, choices=TypeGroup.choices)
+
+
+class GroupElement(models.Model):
+    ...
+
+
+class GroupProduct(models.Model):
+    ...
